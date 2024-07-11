af20719ac1c669e6ef8a520f97d5d99e1db24bb4
hakien <nguyenhakien99@gmail.com>
2024-07-11 16:30:12 +0700

    done

--- src/day19/exercise/baiTap60p/StudentFile1/Application.java
+++ src/day19/exercise/baiTap60p/StudentFile1/Application.java
@@ -9,7 +9,6 @@ public class Application {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         // Input name of file to create
         System.out.println("Input name of file to create: ");
         String fileNameInput = sc.nextLine();
@@ -21,27 +20,41 @@ public class Application {
 
         deleteFile(fileToRead);
 
+        //Get stop index from user
+        int stopIndex = stopIndex();
+
         // Write data to the new file
-        writeFile(fileNameInput, content);
+        writeFile(fileNameInput, content, stopIndex);
 
         System.out.println("Program completed successfully.");
+
+
+
     }
 
-    private static void writeFile(String fileNameInput, List<String> content) {
+    public static void writeFile(String fileNameInput, List<String> content, int stopIndex) {
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(fileNameInput))) {
-            for (String line : content) {
-                bw.write(line);
+            for (int i = 0; i < content.size(); i++) {
+                if(i == stopIndex) {
+                    System.out.println("Pausing for 10 seconds...");
+                    Thread.sleep(10000);
+                    System.out.println("Resuming writing to file...");
+                }
+                bw.write(content.get(i));
                 bw.newLine();
             }
+//            for (String line : content) {
+//                bw.write(line);
+//                bw.newLine();
+//            }
             System.out.println("Data written to file successfully");
-        } catch (IOException e) {
+        } catch (IOException |InterruptedException e) {
             e.printStackTrace();
         }
     }
 
-    private static List<String> readFile(String fileToRead) {
+    public static List<String> readFile(String fileToRead) {
         List<String> content = new ArrayList<>();
-
         try (BufferedReader br = new BufferedReader(new FileReader(fileToRead))) {
             String line;
             while ((line = br.readLine()) != null) {
@@ -53,7 +66,7 @@ public class Application {
         return content;
     }
 
-    private static void createFile(String fileNameInput) {
+    public static void createFile(String fileNameInput) {
         try {
             File file = new File(fileNameInput);
             if (file.createNewFile()) {
@@ -67,12 +80,20 @@ public class Application {
     }
 
     // Optionally delete the original file after writing to new file
-    private static void deleteFile(String fileName) {
+    public static void deleteFile(String fileName) {
         File file = new File(fileName);
         if (file.delete()) {
             System.out.println("Deleted the file: " + fileName);
         } else {
             System.out.println("Failed to delete the file.");
         }
+    }
+
+    public static int stopIndex(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter stop index: ");
+        int stopIndex = scanner.nextInt();
+
+        return stopIndex;
     }
 }
